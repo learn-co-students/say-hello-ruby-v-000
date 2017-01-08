cfb071c3f75023089c10ad294ba01c5a8577d8a9
Sequina Caro <sccaro89@gmail.com>
2017-01-08 23:27:20 +0000

    Done.

--- say_hello.rb
+++ say_hello.rb
@@ -1,6 +1,7 @@
 # Build your say_hello method here
 
-def say_hello(name = "Ruby Programmer")
-  puts "Hello, #{name}!"
+def say_hello(name = "Hello Ruby Programmer")
+  puts "Hello " + name + "!"
 end
-say_hello("Sequina")
+
+say_hello("Kent Beck")
