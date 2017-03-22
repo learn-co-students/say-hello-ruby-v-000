731304a8c035c0b1c7b7e9367e7550912b9d288b
Will Sauer <will@heueso.com>
2017-03-22 22:56:10 +0000

    Done.

--- say_hello.rb
+++ say_hello.rb
@@ -1,1 +1,5 @@
-# Build your say_hello method here
+#method to say hello to a name (if given) or hello to Ruby Programmer is no name given
+
+def say_hello(name = "Ruby Programmer")
+  puts "Hello #{name}!"
+end
