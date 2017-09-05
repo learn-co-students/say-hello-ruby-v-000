8502d68301bc543a766272328013de3cd3e40788
Wayne Buxton <waynebuxton2184@yahoo.com>
2017-09-05 17:48:33 +0000

    Done.

--- say_hello.rb
+++ say_hello.rb
@@ -1,1 +1,19 @@
-# Build your say_hello method here
+# Build your say_hello method her
+def say_hello(name = "Ruby Programmer")
+  puts "Hello #{name}!"
+end
+
+
+
+#say_hello('Kent Beck')
+
+#argument / parameter
+
+#parameters are used when defining a method
+#def some_method(parameter); end
+
+#arguments are when you call that method
+#some_method('Kent') 'kent' is the arg
+
+#default arguments are only assigned to the parameter if the method is called without the argument
+#some_method
