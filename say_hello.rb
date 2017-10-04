3e7f2d15e3fbded12b2fa1ddfe202163d106ff9f
Charity Youngblood <youngblood.charity@gmail.com>
2017-10-04 17:46:51 +0000

    Done.

--- say_hello.rb
+++ say_hello.rb
@@ -1,1 +1,8 @@
 # Build your say_hello method here
+#method should take two arguments - one required and one optional/default
+# be sure to place the default argument at the end of the argument list
+def say_hello(name = "Ruby Programmer")
+  puts "Hello #{name}!"
+end
+
+say_hello
