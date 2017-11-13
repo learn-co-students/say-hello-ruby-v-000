ce3da6386354fd5a73b502309f2df8d682705d03
James Dolan <jwdolan820@gmail.com>
2017-11-13 01:56:01 +0000

    Done.

--- say_hello.rb
+++ say_hello.rb
@@ -1,3 +1,6 @@
 def say_hello(name = "Ruby Programmer")
   puts "Hello #{name}!"
 end
+
+#Line 1: accepts name argument with a default of "Ruby Programmer"
+#Line 2: interpolates the argument into an outputted string
