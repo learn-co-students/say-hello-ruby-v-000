5c8e456385b50c4843bb6ac923e6b7f660a459c2
Liebe Brand <liebebrand@gmail.com>
2018-03-19 05:48:22 +0000

    Done.

--- say_hello.rb
+++ say_hello.rb
@@ -1,1 +1,11 @@
-# Build your say_hello method here
+require_relative '../say_hello.rb'
+
+describe "say_hello" do
+  it "returns the name of the person" do
+    name_of_person = birth_name
+  end
+end
+
+def say_hello(name)
+    puts "Hello, #{name}"
+end
