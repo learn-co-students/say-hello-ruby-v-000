a158d886d4161d5f8695f7c97ba4f84fa2cfe3dc
Kristen Robertson <kristen77@gmail.com>
2018-08-08 02:47:45 +0000

    Done.

--- say_hello.rb
+++ say_hello.rb
@@ -1,1 +1,9 @@
-# Build your say_hello method here
+def say_hello(Kristen, name="Ruby Programmer")
+  puts "Hello #{Kristen)!"
+end
+say_hello
+
+def say_hello(Kristen, name="Ruby Programmer")
+  puts "Hello #(name)!"
+end
+say_hello
