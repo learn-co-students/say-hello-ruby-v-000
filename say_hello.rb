66515d52e87fcdb322cd2f1e8197b45493cbf72b
Brandon Brasson <rocksong622@gmail.com>
2019-01-04 21:14:56 +0000

    Done.

--- say_hello.rb
+++ say_hello.rb
@@ -1,1 +1,11 @@
 # Build your say_hello method here
+
+def say_hello(brandon = "Ruby Programmer")
+  puts "Hello #{brandon}!"
+end
+
+say_hello ("Kent Beck")
+
+#def say_hello()
+ #puts "Hello Ruby Programmer!"
+#end
