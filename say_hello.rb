4e8dad989f231c55c54bebb87296bbef51a71cdf
Lauren Days <lmchambers0420@gmail.com>
2019-02-06 22:03:43 +0000

    Done.

--- say_hello.rb
+++ say_hello.rb
@@ -2,7 +2,6 @@ def say_hello(name)
     puts "Hello  Kent Beck!"
   end
   
-  def say_hello(name="Ruby Programmer!")
+  def say_hello(name="Ruby Programmer")
     puts ("Hello #{name}!")
   end
-  
