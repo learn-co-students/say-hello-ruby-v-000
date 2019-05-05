e99c6dd333680a123b17848427d06686eda7efa2
Manny Georganas <manoli.georganas@gmail.com>
2019-05-05 16:44:58 +0000

    Done.

--- say_hello.rb
+++ say_hello.rb
@@ -1,1 +1,7 @@
 # Build your say_hello method here
+
+# build a method that prints a person's name followed by an exclamation point. If no name specified, it defaults to "Ruby Programmer"
+
+def say_hello(name="Ruby Programmer")
+  puts "Hello " + name + "!"
+end
